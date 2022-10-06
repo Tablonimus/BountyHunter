ae335daf79e32a11dda81cb583de6ed25e2ba27c
tablonimus <lautamartin1995@gmail.com>
2022-10-05 22:49:20 -0300

    Lautaro

--- client/src/components/Game/GameLanding.jsx
+++ client/src/components/Game/GameLanding.jsx
@@ -4,10 +4,10 @@ import { Button } from "flowbite-react";
 import { Link } from "react-router-dom";
 export default function GameLanding() {
   return (
-    <div className="w-full lg:flex lg:flex-row items-center">
+    <div className="w-full lg:flex lg:flex-row items-center px-10">
       <div className="levels">
-        <div className="mt-20 ml-6 flex flex-col text-white text-md w-72 shadow-lg bopacity-90 rounded-md">
-          <h1 className="font-bold bg-black m-1 text-3xl rounded-md opacity-70">
+        <div className="mt-20 ml-6 flex flex-col  items-center text-white text-md w-72 shadow-lg bopacity-90 rounded-md">
+          <h1 className="font-bold bg-black m-1 text-2xl rounded-md opacity-70">
             Easy ⭐
           </h1>
           <h5 className="font-bold bg-black m-1 rounded-md opacity-70">
@@ -28,8 +28,8 @@ export default function GameLanding() {
         </div>
       </div>
       <div className="levels">
-        <div className="mt-20 ml-6 flex flex-col text-white text-md w-72 shadow-lg bopacity-90 rounded-md">
-          <h1 className="font-bold bg-black m-1 text-3xl rounded-md opacity-70">
+        <div className="mt-20 ml-6 flex flex-col items-center text-white text-md w-72 shadow-lg bopacity-90 rounded-md">
+          <h1 className="font-bold bg-black m-1 text-2xl rounded-md opacity-70">
             Medium ⭐⭐⭐
           </h1>
           <h5 className="font-bold bg-black m-1 rounded-md opacity-70">
@@ -39,8 +39,8 @@ export default function GameLanding() {
           <h5 className="font-bold bg-black m-1 rounded-md opacity-70">
             {" "}
             🔴🟡🔴ES: Eres un cazador nato? Rastrea a los criminales mas
-            buscados sin tener conocimiento de sus rasgos físicos. No traduciremos los
-            expedientes por tí. Demuéstranos tu talento!
+            buscados sin tener conocimiento de sus rasgos físicos. No
+            traduciremos los expedientes por tí. Demuéstranos tu talento!
           </h5>
           <div className="m-1">
             <Link to="/game/level2">
@@ -50,7 +50,7 @@ export default function GameLanding() {
         </div>
       </div>
       <div className="levels">
-        <div className="mt-20 ml-6 flex flex-col text-white text-md w-72 shadow-lg bopacity-90 rounded-md">
+        <div className="mt-20 ml-6 flex flex-col items-center text-white text-md w-72 shadow-lg bopacity-90 rounded-md">
           <h1 className="font-bold bg-black m-1 text-2xl rounded-md opacity-70">
             Hard ⭐⭐⭐⭐⭐
           </h1>
